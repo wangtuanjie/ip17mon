61ab5f43708ffd67792fedcbe001fc5acdd185b2
wulinlw <wulinlw@gmail.com>
2015-07-02 13:35:56 +0800

    数据文件更新，免费版没有Isp字段
    数据文件更新，免费版没有Isp字段，
    返回字段处理

--- ip17mon.go
+++ ip17mon.go
@@ -137,14 +137,14 @@ func (loc *Locator) init(data []byte) {
 
 func newLocationInfo(str []byte) *LocationInfo {
 	fields := bytes.Split(str, []byte("\t"))
-	if len(fields) != 5 {
+	if len(fields) != 4 {
 		panic("unexpected ip info:" + string(str))
 	}
 	info := &LocationInfo{
 		Country: string(fields[0]),
 		Region:  string(fields[1]),
 		City:    string(fields[2]),
-		Isp:     string(fields[4]),
+		Isp:     string("1"),
 	}
 
 	if len(info.Country) == 0 {
